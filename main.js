5be3c0c3273d7d47ab9eed45cfa53e31860cb0c2
SergiuszRozycki <smartbastard@icloud.com>
2022-11-05 14:19:26 +0100

    update

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ let userAgent = window.navigator.userAgent;
 let words = userAgent.split(" ");
 let platform = words[1].replace(/[^a-zA-Z ]/g, "");
 console.log(platform);
+
+function useRegexAndroid(userAgent) {
+  let regex = /\bAndroid\b/gm;
+  return regex.test(userAgent);
+}
+
+console.log(useRegexAndroid(userAgent));
+
+// if (platform === "Macintosh" || platform === "iPad" || platform === "iPhone") {
+//     console.log(platform)
+// } else if (platform === "Windows" || platform === "iPad" || platform === "iPhone")
