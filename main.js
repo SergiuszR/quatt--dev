4c18b931c1282453ecbb7e43804a04a142abb17b
SergiuszRozycki <smartbastard@icloud.com>
2022-11-05 14:09:33 +0100

    update

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 let userAgent = window.navigator.userAgent;
-console.log(userAgent);
-console.log(window.navigator);
 let words = userAgent.split(" ");
 let platform = words[1].replace(/[^a-zA-Z ]/g, "");
 console.log(platform);
