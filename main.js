15e0b530070e5cb786a44493d151ba450415bed0
SergiuszRozycki <smartbastard@icloud.com>
2022-11-05 13:38:06 +0100

    update

--- main.js
+++ main.js
@@ -1,27 +1,5 @@
 console.log("Quatt Dev Script");
 
 function getOS() {
-  var userAgent = window.navigator.userAgent,
-    platform =
-      window.navigator?.userAgentData?.platform || window.navigator.platform,
-    macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
-    windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
-    iosPlatforms = ["iPhone", "iPad", "iPod"],
-    // os = null;
-
-  if (macosPlatforms.indexOf(platform) !== -1) {
-    os = "MacOS";
-  } else if (iosPlatforms.indexOf(platform) !== -1) {
-    os = "iOS";
-  } else if (windowsPlatforms.indexOf(platform) !== -1) {
-    os = "Windows";
-  } else if (/Android/.test(userAgent)) {
-    os = "Android";
-  } else if (/Linux/.test(platform)) {
-    os = "Linux";
-  }
-
-  return os;
+  console.log(window.navigator);
 }
-
-alert(getOS());
