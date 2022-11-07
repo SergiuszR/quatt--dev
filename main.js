ea36e96c4e238af2e0c330ae74289f5e159f0e1b
SergiuszRozycki <smartbastard@icloud.com>
2022-11-07 17:55:19 +0100

    update

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ let words = userAgent.split(" ");
 let platform = words[1].replace(/[^a-zA-Z ]/g, "");
 console.log(platform);
 
-function useRegexMacintosh(userAgent) {
-  let regex = /\bMacintosh\b/gm;
-  return regex.test(userAgent);
-}
 function useRegexAndroid(userAgent) {
   let regex = /\bAndroid\b/gm;
   return regex.test(userAgent);
 }
 function useRegexWindows(userAgent) {
   let regex = /\bWindows\b/gm;
+  return regex.test(userAgent);
+}
+function useRegexMacintosh(userAgent) {
+  let regex = /\bMacintosh\b/gm;
   return regex.test(userAgent);
 }
 function useRegexIphone(userAgent) {
